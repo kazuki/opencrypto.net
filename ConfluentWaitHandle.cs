bf6ac39f4c641769689d3d4693b0ed2ed54f3aef
Kazuki Oikawa <k@oikw.org>
2006-11-27 08:10:03 +0000

    * ライセンスをBSDからMIT/X11に変更 * コメントを付加 * 文字コードや改行コードを整理

--- ConfluentWaitHandle.cs
+++ ConfluentWaitHandle.cs
@@ -1,35 +1,34 @@
 // 
 // Copyright (c) 2006, Kazuki Oikawa
-// All rights reserved.
 //
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// * Redistributions of source code must retain the above copyright notice,
-//   this list of conditions and the following disclaimer.
-// * Redistributions in binary form must reproduce the above copyright notice,
-//   this list of conditions and the following disclaimer in the documentation
-//   and/or other materials provided with the distribution.
-// * Neither the name of the author nor the names of its contributors may be used
-//   to endorse or promote products derived from this software
-//   without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 using System;
 using System.Threading;
 
 namespace openCrypto
 {
+	/// <summary>
+	/// <ja>複数のスレッドの終了を待つクラス</ja>
+	/// </summary>
 	class ConfluentWaitHandle
 	{
 		ManualResetEvent _handle;
@@ -41,21 +40,40 @@ namespace openCrypto
 			_counter = 0;
 		}
 
+		/// <summary>
+		/// <ja>終了を待機するスレッドを追加するメソッド</ja>
+		/// </summary>
 		public void StartThread ()
 		{
 			_handle.Reset ();
 			Interlocked.Increment (ref _counter);
 		}
 
+		/// <summary>
+		/// <ja>待機しているスレッドが終了したことを通知するメソッド</ja>
+		/// </summary>
 		public void EndThread ()
 		{
 			if (Interlocked.Decrement (ref _counter) == 0)
 				_handle.Set ();
 		}
 
+		/// <summary>
+		/// <ja>登録されているスレッドがすべて終了するまでブロックするメソッド</ja>
+		/// </summary>
+		/// <returns>
+		/// <ja>trueなら正常にすべてのスレッドが終了、falseならスレッドの終了を待たずにこのインスタンスが閉じられた</ja>
+		/// </returns>
 		public bool WaitOne ()
 		{
 			return _handle.WaitOne ();
+		}
+
+		/// <summary>
+		/// <ja>このインスタンスが保持しているリソースを開放するメソッド</ja>
+		/// </summary>
+		public void Close () {
+			_handle.Close ();
 		}
 	}
 }
--- SymmetricAlgorithmPlus.cs
+++ SymmetricAlgorithmPlus.cs
@@ -1,45 +1,51 @@
 // 
 // Copyright (c) 2006, Kazuki Oikawa
-// All rights reserved.
 //
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// * Redistributions of source code must retain the above copyright notice,
-//   this list of conditions and the following disclaimer.
-// * Redistributions in binary form must reproduce the above copyright notice,
-//   this list of conditions and the following disclaimer in the documentation
-//   and/or other materials provided with the distribution.
-// * Neither the name of the author nor the names of its contributors may be used
-//   to endorse or promote products derived from this software
-//   without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 using System;
 using System.Security.Cryptography;
 
 namespace openCrypto
 {
+	/// <summary>
+	/// <ja>System.Security.Cryptography.SymmetricAlgorithmの拡張クラス。
+	/// 対称アルゴリズムのすべての実装が継承する必要がある、抽象基本クラスを表します。</ja>
+	/// </summary>
 	public abstract class SymmetricAlgorithmPlus : SymmetricAlgorithm
 	{
 		protected int _threads = 1;
 		protected CipherModePlus _mode;
 		protected CipherImplementationType _implType;
 
+		/// <summary>
+		/// <ja>アルゴリズムレベルで並列化に対応しているかを返す</ja>
+		/// </summary>
 		public abstract bool SupportsMultiThread {
 			get;
 		}
 
+		/// <summary>
+		/// <ja>スレッド数を取得または設定します。1を設定すると並列処理を行いません</ja>
+		/// </summary>
 		public int NumberOfThreads {
 			get { return _threads; }
 			set {
@@ -51,16 +57,25 @@ namespace openCrypto
 			}
 		}
 
+		/// <summary>
+		/// <ja>対称アルゴリズムの操作モードを取得または設定します</ja>
+		/// </summary>
 		public override CipherMode Mode {
 			get { return (CipherMode)_mode; }
 			set { _mode = (CipherModePlus)value; }
 		}
 
+		/// <summary>
+		/// <ja>対称アルゴリズムの操作モードを取得または設定します</ja>
+		/// </summary>
 		public CipherModePlus ModePlus {
 			get { return _mode; }
 			set { _mode = value;}
 		}
 
+		/// <summary>
+		/// <ja>利用する実装タイプを取得または設定します</ja>
+		/// </summary>
 		public CipherImplementationType ImplementationType {
 			get { return _implType; }
 			set { _implType = value;}
