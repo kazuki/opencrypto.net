ca00b65b2fa94194a6d3ab3383957548dfa84648
Kazuki Oikawa <k@oikw.org>
2006-12-24 23:53:39 +0000

    2006-12-25 Kazuki Oikawa <kazuki at panicode.com, g031d019 at edu.soft.iwate-pu.ac.jp>
    	* Tests/BlockCipherModeTest.cs: CBC, CFB, OFB, CTRブロック暗号モードのテストを追加

--- Tests/BlockCipherModeTest.cs
+++ Tests/BlockCipherModeTest.cs
@@ -36,9 +36,57 @@ namespace openCrypto.Tests
 		public void ECBTest ()
 		{
 			DummyAlgorithm algo = new DummyAlgorithm ();
+			algo.Mode = CipherMode.ECB;
 			Test (algo, "ECB #1", Helpers.GetRNGBytes (8), Helpers.GetRNGBytes (8),
 					new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 255, 254, 253, 252, 251, 250, 249, 248},
 					new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 255, 254, 253, 252, 251, 250, 249, 248});
+		}
+
+		[Test]
+		public void CBCTest ()
+		{
+			DummyAlgorithm algo = new DummyAlgorithm ();
+			algo.Mode = CipherMode.CBC;
+			Test (algo, "CBC #1", Helpers.GetRNGBytes (8),
+					new byte[] {2, 3, 4, 5, 6, 7, 8, 9},
+					new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 255, 254, 253, 252, 251, 250, 249, 248},
+					new byte[] {3, 1, 7, 1, 3, 1, 15, 1, 255 ^ 3, 254 ^ 1, 253 ^ 7, 252 ^ 1, 251 ^ 3, 250 ^ 1, 249 ^ 15, 248 ^ 1});
+		}
+
+		[Test]
+		public void CFBTest ()
+		{
+			DummyAlgorithm algo = new DummyAlgorithm ();
+			algo.Mode = CipherMode.CFB;
+			Test (algo, "CFB #1", Helpers.GetRNGBytes (8),
+					new byte[] {2, 3, 4, 5, 6, 7, 8, 9},
+					new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 255, 254, 253, 252, 251, 250, 249, 248},
+					new byte[] {2 ^ 1, 3 ^ 2, 4 ^ 3, 5 ^ 4, 6 ^ 5, 7 ^ 6, 8 ^ 7, 9 ^ 8,
+					            255 ^ 2 ^ 1, 254 ^ 3 ^ 2, 253 ^ 4 ^ 3, 252 ^ 5 ^ 4, 251 ^ 6 ^ 5, 250 ^ 7 ^ 6, 249 ^ 8 ^ 7, 248 ^ 9 ^ 8});
+		}
+
+		[Test]
+		public void OFBTest ()
+		{
+			DummyAlgorithm algo = new DummyAlgorithm ();
+			algo.Mode = CipherMode.OFB;
+			Test (algo, "OFB #1", Helpers.GetRNGBytes (8),
+					new byte[] {2, 3, 4, 5, 6, 7, 8, 9},
+					new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 255, 254, 253, 252, 251, 250, 249, 248},
+					new byte[] {2 ^ 1, 3 ^ 2, 4 ^ 3, 5 ^ 4, 6 ^ 5, 7 ^ 6, 8 ^ 7, 9 ^ 8,
+					            255 ^ 2, 254 ^ 3, 253 ^ 4, 252 ^ 5, 251 ^ 6, 250 ^ 7, 249 ^ 8, 248 ^ 9});
+		}
+
+		[Test]
+		public void CTRTest ()
+		{
+			DummyAlgorithm algo = new DummyAlgorithm ();
+			algo.ModePlus = CipherModePlus.CTR;
+			Test (algo, "CTR #1", Helpers.GetRNGBytes (8),
+					new byte[] {0, 0xFF, 1, 0xFF, 2, 0xFF, 3, 0xFF},
+					new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 255, 254, 253, 252, 251, 250, 249, 248},
+					new byte[] {1 ^ 0, 2 ^ 0xFF, 3 ^ 1, 4 ^ 0xFF, 5 ^ 2, 6 ^ 0xFF, 7 ^ 3, 8 ^ 0xFF,
+									255 ^ 0, 254 ^ 0xFF, 253 ^ 1, 252 ^ 0xFF, 251 ^ 2, 250 ^ 0xFF, 249 ^ 4, 248 ^ 0});
 		}
 		
 		static void Test (SymmetricAlgorithmPlus algo, String msg, byte[] key, byte[] iv, byte[] src, byte[] enc)
