0885d972eed928d1a4cf6b1fc853a3e3fbc361c9
Kazuki Oikawa <k@oikw.org>
2007-03-17 02:52:37 +0000

    * リリースに向けて、出力の種類をクラスライブラリに変更 * エントリポイント削除

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("openCrypto.NET")]
-[assembly: AssemblyCopyright ("Copyright (C)  2006")]
+[assembly: AssemblyCopyright("Copyright (C)  2006 Kazuki Oikawa")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
