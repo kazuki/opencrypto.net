1c93148952b21c744d304af0167a2bd7f53ac37f
Kazuki Oikawa <k@oikw.org>
2008-02-29 08:28:42 +0000

    * 各種アルゴリズムの実行テストプログラムを改良

--- Executable/AppLoader.cs
+++ Executable/AppLoader.cs
@@ -42,6 +42,17 @@ namespace openCrypto.Executable
 			return result;
 		}
 
+		static double[] Run (SymmetricAlgorithmPlus algo, CipherModePlus mode, int keySize, int blockSize, int dataSize, int threads)
+		{
+			double[] result = SpeedTest.Run (algo, mode, keySize, blockSize, dataSize, threads);
+			for (int i = 0; i < 10; i++) {
+				double[] temp = SpeedTest.Run (algo, mode, keySize, blockSize, dataSize, threads);
+				result[0] = Math.Max (result[0], temp[0]);
+				result[1] = Math.Max (result[1], temp[1]);
+			}
+			return result;
+		}
+
 		static double[] Run (ECDomainNames domain)
 		{
 			ECDSAManaged ecdsa = new ECDSAManaged (domain);
@@ -58,16 +69,43 @@ namespace openCrypto.Executable
 		static void Main ()
 		{
 			CipherMode mode = CipherMode.ECB;
-			int keySize = 128, blockSize = 128, dataSize = 1024 * 1024 * 8;
+			int dataSize = 1024 * 1024;
 			double[] result;
 
-			result = Run (new CamelliaManaged (), mode, keySize, blockSize, dataSize);
-			Console.WriteLine ("Camellia Encrypt: {0}Mbps, Decrypt: {1}Mbps", result[0], result[1]);
+			Console.WriteLine ("Symmetric-Key Algorithm:");
+			result = Run (new CamelliaManaged (), mode, 128, 128, dataSize);
+			Console.WriteLine ("Camellia 128bit Encrypt: {0:f2}Mbps, Decrypt: {1:f2}Mbps", result[0], result[1]);
+			result = Run (new CamelliaManaged (), mode, 192, 128, dataSize);
+			Console.WriteLine ("Camellia 192bit Encrypt: {0:f2}Mbps, Decrypt: {1:f2}Mbps", result[0], result[1]);
+			result = Run (new CamelliaManaged (), mode, 256, 128, dataSize);
+			Console.WriteLine ("Camellia 256bit Encrypt: {0:f2}Mbps, Decrypt: {1:f2}Mbps", result[0], result[1]);
 
-			result = Run (new RijndaelManaged (), mode, keySize, blockSize, dataSize);
-			Console.WriteLine ("Rijndael Encrypt: {0}Mbps, Decrypt: {1}Mbps", result[0], result[1]);
+			result = Run (new RijndaelManaged (), mode, 128, 128, dataSize);
+			Console.WriteLine ("Rijndael 128bit Encrypt: {0:f2}Mbps, Decrypt: {1:f2}Mbps", result[0], result[1]);
+			result = Run (new RijndaelManaged (), mode, 192, 128, dataSize);
+			Console.WriteLine ("Rijndael 192bit Encrypt: {0:f2}Mbps, Decrypt: {1:f2}Mbps", result[0], result[1]);
+			result = Run (new RijndaelManaged (), mode, 256, 128, dataSize);
+			Console.WriteLine ("Rijndael 256bit Encrypt: {0:f2}Mbps, Decrypt: {1:f2}Mbps", result[0], result[1]);
 
-			for (int i = (int)ECDomainNames.secp112r1; i <= (int)ECDomainNames.secp521r1; i ++) {
+			if (Environment.ProcessorCount > 1) {
+				result = Run (new CamelliaManaged (), CipherModePlus.ECB, 128, 128, dataSize, 2);
+				Console.WriteLine ("Camellia 128bit (2-threads) Encrypt: {0:f2}Mbps, Decrypt: {1:f2}Mbps", result[0], result[1]);
+
+				result = Run (new RijndaelManaged (), CipherModePlus.ECB, 128, 128, dataSize, 2);
+				Console.WriteLine ("Rijndael 128bit (2-threads) Encrypt: {0:f2}Mbps, Decrypt: {1:f2}Mbps", result[0], result[1]);
+
+				if (Environment.ProcessorCount != 2) {
+					result = Run (new CamelliaManaged (), CipherModePlus.ECB, 128, 128, dataSize, Environment.ProcessorCount);
+					Console.WriteLine ("Camellia 128bit ({2}-threads) Encrypt: {0:f2}Mbps, Decrypt: {1:f2}Mbps", result[0], result[1], Environment.ProcessorCount);
+
+					result = Run (new RijndaelManaged (), CipherModePlus.ECB, 128, 128, dataSize, Environment.ProcessorCount);
+					Console.WriteLine ("Rijndael 128bit ({2}-threads) Encrypt: {0:f2}Mbps, Decrypt: {1:f2}Mbps", result[0], result[1], Environment.ProcessorCount);
+				}
+			}
+			Console.WriteLine ();
+
+			Console.WriteLine ("ECDSA:");
+			for (int i = (int)ECDomainNames.secp112r1; i <= (int)ECDomainNames.secp521r1; i++) {
 				ECDomainNames domain = (ECDomainNames)i;
 				result = Run (domain);
 				Console.WriteLine ("{0}: Sign: {1}ms, Verify: {2}ms", domain, result[0], result[1]);
